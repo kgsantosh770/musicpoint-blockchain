adf31d7a41368a3ad6839680902d38f53ec75a64
santosh <santhoshkrishna770@gmail.com>
2022-09-16 22:53:07 +0530

    blockchain-music-point

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FormEvent, useState, useEffect } from 'react';
+import "./scss/App.scss";
+import LinkInput from "./components/LinkInput";
+import MusicBox from './components/MusicBox';
+import ConnectToWallet from './components/ConnectToWalletButton';
+import { useWalletContext } from "./WalletContext"
+import { ContractPostMusic, ContractGetTotalMusic } from "./contracts/MusicPointFunctions"
+declare var window: any;
 
 function App() {
+
+  const { currentAccount } = useWalletContext()
+
+  interface Music {
+    owner: string,
+    musicUrl: string,
+  }
+
+  interface AllMusic extends Array<Music> { }
+
+  const [allMusicData, setAllMusicData] = useState<AllMusic>([]);
+  const [totalMusic, setTotalMusic] = useState(0);
+
+  useEffect(()=>{
+    ContractGetTotalMusic()
+      .then(res => setTotalMusic(res))
+  },[])
+
+  const musicBoxes = allMusicData.map((data, i) =>
+    <MusicBox key={i} owner={data.owner} musicUrl={data.musicUrl} />
+  )
+
+  function handlePostMusic(event: FormEvent, _owner: string, _musicUrl: string) {
+    event.preventDefault();
+    const newMusicObject = {
+      owner: _owner,
+      musicUrl: _musicUrl,
+    }
+    ContractPostMusic(_musicUrl)
+      .then(() => setAllMusicData([newMusicObject, ...allMusicData]))
+      .then(() => setTotalMusic(prevCount => prevCount+1))
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>👋 Hey everyone!</h1>
+      <div className="bio">
+        I am Santosh. This is my first blockchain project. Send a music link to me :)
+      </div>
+      <div className='total-music'>
+        <span className="count">
+          {totalMusic}
+        </span>
+        members has shared music here !!!!
+      </div>
+      <ConnectToWallet />
+      {currentAccount.length !== 0 &&
+        <>
+          <LinkInput handleClick={handlePostMusic} />
+          {musicBoxes}
+        </>
+      }
     </div>
   );
 }
---
+++ src/dummyData.tsx
@@ -0,0 +1,14 @@
+export const data = [
+    {
+        owner: "0x3432kj4g6hg46jpojm65k4bnv5hg6j4kv7n54lk76v54iuh",
+        musicUrl: "https://dummyurl.com/mymusic"
+    },
+    {
+        owner: "0x3432kj4g6hg46jpojm65k4bnv5hg6j4kv7n54lk76v54iuh",
+        musicUrl: "https://dummyurl.com/mymusic"
+    },
+    {
+        owner: "0x3432kj4g6hg46jpojm65k4bnv5hg6j4kv7n54lk76v54iuh",
+        musicUrl: "https://dummyurl.com/mymusic"
+    }
+]
